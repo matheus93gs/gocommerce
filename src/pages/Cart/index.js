54eb919f9900ad96c80b4940414036778c63b0cd
gr8matheus <matheus93gs@hotmail.com>
2020-02-26 15:50:14 -0300

    O mesmo produto aparecer somente uma vez no cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -7,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +22,40 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
-                alt="Bike"
-              />
-            </td>
-            <td>
-              <strong>Bike muito boa</strong>
-              <span>R$2500,00</span>
-            </td>
-            <td>
-              <div>
+          {cart.map(product =>(
+            <tr>
+              <td>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#f58634" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#f58634" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$3000,00</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#f58634" />
+                  <MdDelete size={20} color="#f58634" />
                 </button>
-                <input type="number" readOnly value={2} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#f58634" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$3000,00</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#f58634" />
-              </button>
-            </td>
+              </td>
           </tr>
+          ))}
+
         </tbody>
       </ProductTable>
 
@@ -66,3 +70,9 @@ export default function Cart() {
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
