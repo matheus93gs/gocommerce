9f7f6b581968471c5289adaabf1924e5301970bd
gr8matheus <matheus93gs@hotmail.com>
2020-02-26 08:53:43 -0300

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/bicicleta-gts-aro-29-freio-a-disco-cambio-traseiro-gtsm1-tsi-21-marchas-e-amortecedor-ride/02/ABS-0241-002/ABS-0241-002_zoom2.jpg?ims=326x"
+          alt="bike"
+        />
+        <strong>Bike legal</strong>
+        <span>R$2500,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
